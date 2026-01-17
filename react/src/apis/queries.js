78d8720111c43fe339d86a92fc3c7df130ab79ef
rahulkulkarni <rahulkulkarni375@gmail.com>
2026-01-17 23:48:15 +0530

    feat: add create todo

--- react/src/apis/queries.js
+++ react/src/apis/queries.js
@@ -1,9 +1,33 @@
-export async function getTodos(){
-    try{
+export async function getTodos() {
+    try {
         const response = await fetch("http://localhost:3000/api/to/all")
         const result = response.json()
         return result;
-    }catch(err){
-        console.log("Error while fetching todos : ",err)
+    } catch (err) {
+        console.log("Error while fetching todos : ", err)
     }
 }
+
+export async function createTodos(newTodo) {
+    console.log("Called create Todo");
+    
+    try {
+        const response = await fetch("http://localhost:3000/api/to/create-todo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({newTodo}),
+        });
+
+        // if (!response.ok) {
+        //     throw new Error("Failed to create todo");
+        // }
+
+        const result = await response.json();
+
+    } catch (err) {
+        console.log("Error while fetching todos : ", err)
+    }
+}
+

171202b31a35d8e77ef4d496cfb21a76204ffc9e
rahulkulkarni <rahulkulkarni375@gmail.com>
2026-02-03 23:49:18 +0530

    feat(routes): add create, update & delete

--- node/routes/TodoRoute.js
+++ node/routes/TodoRoute.js
@@ -4,5 +4,7 @@ const getTodoController = require("../controllers/TodoController");
 
 router.get("/all", getTodoController.getTodo); 
 router.post("/create-todo", getTodoController.postTodo); 
+router.patch("/patch-todo", getTodoController.patchTodo); 
+router.delete("/delete-todo", getTodoController.deleteTodo); 
 
 module.exports = router;

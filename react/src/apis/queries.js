69720f31d92cc608b56ea7162d43e246c25590ee
rahulkulkarni <rahulkulkarni375@gmail.com>
2026-02-03 23:42:42 +0530

    feat: added apis for delete & patch

--- react/src/apis/queries.js
+++ react/src/apis/queries.js
@@ -9,8 +9,6 @@ export async function getTodos() {
 }
 
 export async function createTodos(newTodo) {
-    console.log("Called create Todo");
-    
     try {
         const response = await fetch("http://localhost:3000/api/to/create-todo", {
             method: "POST",
@@ -20,14 +18,60 @@ export async function createTodos(newTodo) {
             body: JSON.stringify({newTodo}),
         });
 
-        // if (!response.ok) {
-        //     throw new Error("Failed to create todo");
-        // }
+        if (!response.ok) {
+            throw new Error("Failed to create todo");
+        }
 
         const result = await response.json();
+        console.log("Create Res : ",response)
+        return result
 
     } catch (err) {
         console.log("Error while fetching todos : ", err)
     }
 }
 
+export async function patchTodos(updateTodo) {
+    console.log('update Todo : ',updateTodo);
+    
+    try {
+        const response = await fetch("http://localhost:3000/api/to/patch-todo", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({updateTodo}),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to update/patch todo");
+        }
+
+        const result = await response.json();
+        return result
+
+    } catch (err) {
+        console.log("Error while updating todos : ", err)
+    }
+}
+
+export async function deleteTodos() {
+    try {
+        const response = await fetch("http://localhost:3000/api/to/delete-todo", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to delete todo");
+        }
+
+        const result = await response.json();
+        return result
+
+    } catch (err) {
+        console.log("Error while deleting todos : ", err)
+    }
+}

d8143c39c456973537317fb81dc8a3be3c419bdc
rahulkulkarni <rahulkulkarni375@gmail.com>
2026-02-03 23:49:59 +0530

    feat(controllers): add create, update & delete

--- node/controllers/TodoController.js
+++ node/controllers/TodoController.js
@@ -2,8 +2,10 @@ const todoService = require("../services/TodoServices");
 
 async function getTodo(req, res) {
   try {
-    const result = await todoService.getTodo();
-    res.status(500).json(result);
+    const allTodosNcount = await todoService.getTodo();
+    const result = allTodosNcount.result
+    const count = allTodosNcount.count
+    res.status(200).json({result,count});
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching the getTodo " });
   }
@@ -13,10 +15,30 @@ async function postTodo(req, res) {
   try {
     const { newTodo } = req.body;
     const result = await todoService.postTodo(newTodo);
-    res.status(500).json(result);
+    res.status(201).json(result);
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching the postTodo " });
   }
 }
 
-module.exports = { getTodo, postTodo };
+async function patchTodo(req, res) {
+  try {
+    const { updateTodo } = req.body;
+    const result = await todoService.patchTodo(updateTodo);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching the patchTodo " });
+  }
+}
+
+async function deleteTodo(req, res) {
+  try {
+    const result = await todoService.deleteTodo();
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while deleting Todos " });
+  }
+}
+
+
+module.exports = { getTodo, postTodo, patchTodo, deleteTodo };

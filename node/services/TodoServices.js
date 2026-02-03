908945d18d3dc79e9845741bf0f9dcd952b6fe4a
rahulkulkarni <rahulkulkarni375@gmail.com>
2026-02-03 23:57:18 +0530

    feat(services): add create, update & delete todos

--- node/services/TodoServices.js
+++ node/services/TodoServices.js
@@ -3,10 +3,13 @@ const pool = require("../config/db");
 async function getTodo() {
   try {
     console.log("In getTodo Service")
+    // CALL count_completed_todos(NULL) this in nodejs backend
     const query = `select * from todos;`;
     const result = await pool.query(query);
+    const execProcedure = `CALL count_completed_todos($1, $2);`;
+    const proceedureRes = await pool.query(execProcedure, [null, null]);
 
-    return result.rows;
+    return { result: result.rows, count: proceedureRes.rows };
   } catch (error) {
     console.error("Error getting getTodo Service :", error);
     throw error;
@@ -26,6 +29,34 @@ async function postTodo(newTodo) {
   }
 }
 
+async function patchTodo(updateTodo) {
+  try {
+    console.log("In patchTodo Service : ", updateTodo)
+    const query = `UPDATE todos
+                SET completed = ${updateTodo.status}
+                WHERE id = ${updateTodo.id};`;
+
+    const result = await pool.query(query);
+    return result.rowCount;
+  } catch (error) {
+    console.error("Error getting patchTodo Service :", error);
+    throw error;
+  }
+}
+
+async function deleteTodo() {
+  try {
+    console.log("In deleteTodo Service : ")
+    const query = `DELETE FROM todos WHERE completed = true RETURNING *`;
+    const result = await pool.query(query);
+    return result.rowCount;
+  } catch (error) {
+    console.error("Error getting deleteTodo Service :", error);
+    throw error;
+  }
+}
 
 
-module.exports = { getTodo, postTodo }
+
+module.exports = { getTodo, postTodo, patchTodo, deleteTodo }
+
